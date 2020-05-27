bb7c9d9a95e8d4badb9de613fa431426330f7a8e
ad3bay0 <byoextreme@hotmail.com>
2020-05-27 23:39:10 +0100

    chore: init commit

---
+++ src/test/java/com/ad3bay0/apex/service/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad3bay0.apex.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
